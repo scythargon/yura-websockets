f5b273c3198a969a5fa95d69b131fb543e588c7c
Dmitry Vasilev <scythargon@gmail.com>
2021-03-13 23:33:27 +0300

    Add websockets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,52 @@
+import React, {useEffect} from 'react';
+import {useHotkeys} from "react-hotkeys-hook";
 
-function App() {
+import ReconnectingWebSocket from 'reconnecting-websocket';
+
+
+const App = () => {
+
+  let username = "";
+  let ws = null;
+
+  const onOpen = (e) => {
+    console.log("Websocket connection established.");
+  };
+
+  const onClose = (e) => {
+    console.error("Websocket connection closed");
+  };
+
+  // You can tell that's your message here if it has the same username.
+  const onMessage = (e) => {
+    const data = JSON.parse(e.data);
+    console.log("New message:", data);
+  };
+
+  const sendMessage = (messageText) => {
+    ws.send(
+      JSON.stringify({
+        fromUser: username,
+        text: messageText,
+      })
+    );
+  };
+
+  // Something like an object constructor - executed only once.
+  useEffect(() => {
+    username = prompt("Enter your nickname:", "");
+
+    ws = new ReconnectingWebSocket("ws://localhost:4000");
+    ws.onopen = onOpen;
+    ws.onmessage = onMessage;
+    ws.onclose = onClose;
+  }, []);
+
+  useHotkeys('up', () => sendMessage('up'));
+  useHotkeys('left', () => sendMessage('left'));
+  useHotkeys('down', () => sendMessage('down'));
+  useHotkeys('right', () => sendMessage('right'));
+
   return (
     <div className="App">
       <header className="App-header">
--- main.py
+++ main.py
@@ -1,38 +1,39 @@
-import asyncio 
-import logging 
-import websockets 
-from websockets import WebSocketServerProtocol, WebSocketClientProtocol
+import asyncio
+import logging
+import websockets
 
-logging.basicConfig(level=logging.INFO) 
-
-
-class Server: 
-    clients = set() 
-    async def register(self, ws: WebSocketServerProtocol) -> None: 
-        self.clients.add(ws) 
-        logging.info(f'fws.remote_addressl connects.') 
-
-    async def unregister(self, ws: WebSocketServerProtocol) -> None: 
-        self.clients.remove(ws) 
-        logging.info(f'fws.remote_addressl disconnects.') 
-
-    async def send_to_clients(self, message: str) -> None: 
-        if self.clients: 
-            await asyncio.wait([client.send(message) for client in self.clients]) 
-
-    async def ws_handler(self, ws: WebSocketServerProtocol, uri: str) -> None: 
-        await self.register(ws) 
-        try: 
-            await self.distribute(ws) 
-        finally: 
-            await self.unregister(ws) 
-
-    async def distribute(self, ws: WebSocketServerProtocol) -> None: 
-        async for message in ws: await self.send_to_clients(message) 
+logging.basicConfig(level=logging.INFO)
 
 
-server = Server() 
-start_server = websockets.serve(server.ws_handler, 'localhost', 4000) 
-loop = asyncio.get_event_loop() 
-loop.run_until_complete(start_server) 
-loop.run_forever() 
+class Server:
+    clients = set()
+
+    async def register(self, ws) -> None:
+        self.clients.add(ws)
+        logging.info(f'{ws.remote_address} connects.')
+
+    async def unregister(self, ws) -> None:
+        self.clients.remove(ws)
+        logging.info(f'{ws.remote_address} disconnects.')
+
+    async def send_to_clients(self, message: str) -> None:
+        if self.clients:
+            await asyncio.wait([client.send(message) for client in self.clients])
+
+    async def ws_handler(self, ws, uri: str) -> None:
+        await self.register(ws)
+        try:
+            await self.distribute(ws)
+        finally:
+            await self.unregister(ws)
+
+    async def distribute(self, ws) -> None:
+        async for message in ws:
+            await self.send_to_clients(message)
+
+
+server = Server()
+start_server = websockets.serve(server.ws_handler, 'localhost', 4000)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_server)
+loop.run_forever()
